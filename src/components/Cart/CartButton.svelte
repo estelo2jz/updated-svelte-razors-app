25600758e31fed36c04b1a878e84ab6fde0f1110
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2020-03-14 13:03:52 -0500

    added cart logo

--- src/components/Cart/CartButton.svelte
+++ src/components/Cart/CartButton.svelte
@@ -1,16 +1,25 @@
 <script>
   import globalStore from '../../stores/globalStore';
+  import FaShoppingCart from 'svelte-icons/fa/FaShoppingCart.svelte';
 </script>
 
 <!-- cart toggle -->
-<div class="btn-cart-container">
+<div class="btn-cart-container icon">
   <button 
     class="btn-cart-toggle" 
     on:click={() => {
       console.log('hello');
       globalStore.toggleItem('cart', true);
     }}>
-    <i class="fas fa-cart-plus" />
+    <FaShoppingCart />
   </button>
   <span class="btn-cart-items">100</span>
 </div>
+
+<style>
+  .icon {
+    color: red;
+    width: 312px;
+    height: 312px;
+  }
+</style>
