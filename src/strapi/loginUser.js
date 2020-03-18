9b7d6660fe6d484aefebc62b62f63d8317a39c14
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2020-03-18 18:10:56 -0500

    update with api ser login function and localStorage

---
+++ src/strapi/loginUser.js
@@ -0,0 +1,16 @@
+import axios from 'axios';
+import url from './URL';
+
+async function loginUser({email, password}) {
+  const response = await axios.post(`${url}/auth/local`, {
+    identifier: email, password
+  }).catch(error => console.log(error));
+
+  if (response) {
+    // setupUser(response)
+  }
+
+  return response;
+} 
+
+export default loginUser;
